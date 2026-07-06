80e9e73bf8e597e48df252deb0becb7e884fbf1d
koike0311 <172023158+koike0311@users.noreply.github.com>
2026-07-06 10:32:43 +0900

    課題のプログラム作成

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -1,7 +1,22 @@
 package org.example;
 
 public class Calculate {
-  public int sum(int x, int y) {
-    return x + y;
+
+  public int getIndividualFee(int age) {
+    if (age <= 12) {
+      return 1000;
+    } else {
+      return 2000;
+    }
+  }
+
+  public int getTotalFee(int age, int groupSize) {
+    int total = getIndividualFee(age) * groupSize;
+
+    if (groupSize >= 10) {
+      total = (int) (total * 0.9);
+    }
+
+    return total;
   }
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -4,11 +4,28 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CalculateTest {
+
   @Test
-  public void testSum() {
-    Calculate calculate = new Calculate();
-    int expected = 5;
-    assertEquals(expected, calculate.sum(2, 3));
+  public void testGetIndividualFeeChild() {
+    Calculate calc = new Calculate();
+    assertEquals(1000, calc.getIndividualFee(10));
   }
 
+  @Test
+  public void testGetIndividualFeeAdult() {
+    Calculate calc = new Calculate();
+    assertEquals(2000, calc.getIndividualFee(25));
+  }
+
+  @Test
+  public void testGetTotalFeeWithoutDiscount() {
+    Calculate calc = new Calculate();
+    assertEquals(1000, calc.getTotalFee(10, 1));
+  }
+
+  @Test
+  public void testGetTotalFeeWithDiscount() {
+    Calculate calc = new Calculate();
+    assertEquals(27000, calc.getTotalFee(25, 15));
+  }
 }
